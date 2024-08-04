54a99edeb8ba397df15b20a863d26707e56c115f
Rocket5444 <127768624+Rocket5444@users.noreply.github.com>
2024-08-05 00:13:09 +0530

    Delete Chapter10 directory

--- Chapter10/oop.py
+++
@@ -1,8 +0,0 @@
-'''
-Solving a problem by creating a object is one of the most popular approaches in
-programming. This is called oject-oriented programing.
-
-This concept focuses on using reusable code.
-'''
-
-# Class is blueprint for creating object.
